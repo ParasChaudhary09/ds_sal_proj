cba0f6a05e1b8cdf635bc23d6a4cc9ad867fd0cd
Paras Chaudhary <32906980+ParasChaudhary09@users.noreply.github.com>
2020-10-17 21:01:24 +0530

    Created FlaskAPI pint

---
+++ FlaskAPI/request.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 19:34:20 2020
+
+@author: Ken
+"""
+
+
+import requests 
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL,headers=headers, json=data) 
+
+r.json()
+
